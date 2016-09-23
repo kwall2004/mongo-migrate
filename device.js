60f64f4aaae584822f01431fc3672339499888f7
kwall2004 <kwall2004@gmail.com>
2016-09-23 15:00:19 -0400

    fix

--- device.js
+++ device.js
@@ -1,38 +1,44 @@
 var MongoClient = require('mongodb').MongoClient;
+// var uri = 'mongodb://localhost:27017/vision2';
+var uri = 'mongodb://heroku_9d3ppsr0:a706flgp82q7qenmd166qmvq8d@ds051655.mlab.com:51655/heroku_9d3ppsr0';
 
-MongoClient.connect('mongodb://localhost:27017/Vision', function (err, db) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+MongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log('connected');
-    var clients = db.collection('Client');
-    var devices = db.collection('Device');
-    devices.find().forEach(
-        function (device) {
-            clients.findOne({ OldId: device.ClientId }, function (err, client) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
+  console.log('connected');
+  var clients = db.collection('clients');
+  var devices = db.collection('devices');
+  devices.find().forEach(
+    function (device) {
+      clients.findOne({ OldId: device.ClientId }, function (err, client) {
+        if (err) {
+          console.log(err);
+          return;
+        }
 
-                if (client) {
-                    devices.updateOne({ _id: device._id }, { $set: { ClientId: client._id } });
-                    console.log(device.ClientId + ' -> ' + client._id);
-                }
-            });
-
-            devices.updateOne({ _id: device._id }, { $set: { IMEI: device.IMEI.toString() }});
-            devices.updateOne({ _id: device._id }, { $set: { SerialNumber: device.SerialNumber.toString() }});
-            devices.updateOne({ _id: device._id }, { $set: { GroupId: parseInt(device.GroupId) }});
-        },
-        function (err) {
+        if (client) {
+          devices.updateOne({ _id: device._id }, { $set: { ClientId: client._id } }, function (err, result) {
             if (err) {
-                console.log(err);
+              console.log(err);
+              return;
             }
 
-            db.close();
+            console.log(result.result);
+          });
         }
-    );
+      });
+
+      devices.updateOne({ _id: device._id }, { $set: { IMEI: device.IMEI.toString() }});
+      devices.updateOne({ _id: device._id }, { $set: { SerialNumber: device.SerialNumber.toString() }});
+      devices.updateOne({ _id: device._id }, { $set: { GroupId: parseInt(device.GroupId) }});
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 })
--- vehicle.js
+++ vehicle.js
@@ -1,34 +1,40 @@
 var MongoClient = require('mongodb').MongoClient;
+// var uri = 'mongodb://localhost:27017/vision2';
+var uri = 'mongodb://heroku_9d3ppsr0:a706flgp82q7qenmd166qmvq8d@ds051655.mlab.com:51655/heroku_9d3ppsr0';
 
-MongoClient.connect('mongodb://localhost:27017/vision2', function (err, db) {
-    if (err) {
-        console.log(err);
-        return;
-    }
+MongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log('connected');
-    var clients = db.collection('clients');
-    var vehicles = db.collection('vehicles');
-    vehicles.find().forEach(
-        function (vehicle) {
-            clients.findOne({ OldId: vehicle.ClientId }, function (err, client) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
+  console.log('connected');
+  var clients = db.collection('clients');
+  var vehicles = db.collection('vehicles');
+  vehicles.find().forEach(
+    function (vehicle) {
+      clients.findOne({ OldId: vehicle.ClientId }, function (err, client) {
+        if (err) {
+          console.log(err);
+          return;
+        }
 
-                if (client) {
-                    vehicles.updateOne({ _id: vehicle._id }, { $set: { ClientId: client._id } });
-                    console.log(vehicle.ClientId + ' -> ' + client._id);
-                }
-            });
-        },
-        function (err) {
+        if (client) {
+          vehicles.updateOne({ _id: vehicle._id }, { $set: { ClientId: client._id } }, function (err, result) {
             if (err) {
-                console.log(err);
+              console.log(err);
+              return;
             }
 
-            db.close();
+            console.log(result.result);
+          });
         }
-    );
-})
+      });
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+});
