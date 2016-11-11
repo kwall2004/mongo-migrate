ab134e6c5ccce3cf3358cd667b9f07459962b151
kwall2004 <kwall2004@gmail.com>
2016-11-11 16:36:07 -0500

    update

--- device.js
+++ device.js
@@ -15,95 +15,108 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT d.DvceID, d.BsnsInfoID, d.IMEI, d.GrupID, d.FWVrsn, d.ConfVrsn, d.SrlNum, m.VhclID, unix_timestamp(m.StrtDate) AS StrtDate, unix_timestamp(m.EndDate) AS EndDate ' +
+connection.query('SELECT d.DvceID, d.IMEI, d.GrupID, d.FWVrsn, d.ConfVrsn, d.SrlNum, ui.BsnsInfoID, m.VhclID, unix_timestamp(m.StrtDate) AS StrtDate, unix_timestamp(m.EndDate) AS EndDate ' +
   'FROM Dvce d ' +
-  'LEFT JOIN UserVhclDvceMap m ON m.DvceID = d.DvceID ', function (err, rows, fields) {
+  'LEFT JOIN UserVhclDvceMap m ON m.DvceID = d.DvceID ' +
+  'LEFT JOIN UserInfo ui ON ui.UserInfoID = m.UserInfoID ', function (err, rows, fields) {
     if (err) throw err;
 
-    MongoClient.connect(uri).then(function (db) {
-      var clients = db.collection('clients');
-      var vehicles = db.collection('vehicles');
-      var devices = db.collection('devices');
+    MongoClient.connect(uri)
+      .then(function (db) {
+        var clients = db.collection('clients');
+        var vehicles = db.collection('vehicles');
+        var devices = db.collection('devices');
 
-      Promise.each(rows, function (row) {
-        return devices.findOne({ oldId: parseInt(row.DvceID) }).then(function (device) {
-          if (device) {
-            return vehicles.findOne({ oldId: parseInt(row.VhclID) }).then(function (vehicle) {
-              if (vehicle) {
-                return devices.updateOne({ _id: device._id }, {
-                  $push: {
-                    vehicles: {
-                      id: ObjectID(vehicle._id),
-                      startDate: new Date(row.StrtDate * 1000),
-                      endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+        return Promise.each(rows, function (row) {
+          return devices.findOne({ oldId: parseInt(row.DvceID) })
+            .then(function (device) {
+              if (device) {
+                return clients.findOne({ oldId: parseInt(row.BsnsInfoID) })
+                  .then(function (client) {
+                    if (client) {
+                      return devices.updateOne({ _id: device._id }, {
+                        $push: {
+                          clients: {
+                            id: ObjectID(client._id),
+                            startDate: new Date(row.StrtDate * 1000),
+                            endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+                          }
+                        }
+                      });
                     }
-                  }
+                  })
 
-                }).then(function (result) {
-                  console.log(row.DvceID, result.result);
+                  .then(function () {
+                    return vehicles.findOne({ oldId: parseInt(row.VhclID) });
+                  })
 
-                }).catch(function (err) {
-                  throw err;
-                })
-              }
-            });
-          }
-          else {
-            var doc = {
-              oldId: row.DvceID,
-              clientId: null,
-              imei: row.IMEI.toString(),
-              groupId: parseInt(row.GrupID),
-              firmwareVersion: row.FWVrsn,
-              configVersion: row.ConfVrsn,
-              serialNumber: row.SrlNum ? row.SrlNum.toString() : null
-            };
-
-            return clients.findOne({ oldId: parseInt(row.BsnsInfoID) }).then(function (client) {
-              if (client) {
-                doc.clientId = ObjectID(client._id);
-              }
-
-              return vehicles.findOne({ oldId: parseInt(row.VhclID) }).then(function (vehicle) {
-                if (vehicle) {
-                  if (!doc.vehicles) doc.vehicles = [];
-
-                  doc.vehicles.push({
-                    id: ObjectID(vehicle._id),
-                    startDate: new Date(row.StrtDate * 1000),
-                    endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+                  .then(function (vehicle) {
+                    if (vehicle) {
+                      return devices.updateOne({ _id: device._id }, {
+                        $push: {
+                          vehicles: {
+                            id: ObjectID(vehicle._id),
+                            startDate: new Date(row.StrtDate * 1000),
+                            endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+                          }
+                        }
+                      });
+                    }
                   });
-                }
+              }
+              else {
+                var doc = {
+                  oldId: row.DvceID,
+                  imei: row.IMEI.toString(),
+                  groupId: parseInt(row.GrupID),
+                  firmwareVersion: row.FWVrsn,
+                  configVersion: row.ConfVrsn,
+                  serialNumber: row.SrlNum ? row.SrlNum.toString() : null
+                };
 
-                return devices.insertOne(doc).then(function (result) {
-                  console.log(row.DvceID, result.result);
+                return clients.findOne({ oldId: parseInt(row.BsnsInfoID) })
+                  .then(function (client) {
+                    if (client) {
+                      if (!doc.clients) doc.clients = [];
 
-                }).catch(function (err) {
-                  throw err;
-                });
+                      doc.clients.push({
+                        id: ObjectID(client._id),
+                        startDate: new Date(row.StrtDate * 1000),
+                        endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+                      })
+                    }
 
-              }).catch(function (err) {
-                throw err;
-              });
+                    return vehicles.findOne({ oldId: parseInt(row.VhclID) })
+                  })
 
-            }).catch(function (err) {
-              throw err;
+                  .then(function (vehicle) {
+                    if (vehicle) {
+                      if (!doc.vehicles) doc.vehicles = [];
+
+                      doc.vehicles.push({
+                        id: ObjectID(vehicle._id),
+                        startDate: new Date(row.StrtDate * 1000),
+                        endDate: row.EndDate ? new Date(row.EndDate * 1000) : null
+                      });
+                    }
+
+                    return devices.insertOne(doc)
+                  });
+              }
+            })
+
+            .then(function (result) {
+              console.log(row.DvceID, result.result);
             });
-          }
+        })
 
-        }).catch(function (err) {
-          throw err;
-        });
+          .then(function (result) {
+            console.log('done');
+            return;
+          })
+      })
 
-      }).then(function (result) {
-        console.log('done');
-        return;
-
-      }).catch(function (err) {
+      .catch(function (err) {
         throw err;
       });
-
-    }).catch(function (err) {
-      throw err;
-    });
   });
